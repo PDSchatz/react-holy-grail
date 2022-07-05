4600474497fd1c181940e4f6159a9cfb7e630b96
gucci <pdschatz@gmail.com>
2022-07-05 14:08:39 -0400

    done?

---
+++ src/components/content.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Content() {
+  return (
+    <main>
+      some main content
+    </main>
+  )
+}
+
+export default Content
---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      some header content
+    </header>
+  )
+}
+
+export default Header
